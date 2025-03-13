dabd95d7ef67116eb6b2f99bc4a3d750217d2d38
Mohana Sri <mohanasri2606@gmail.com>
2025-03-13 18:17:05 +0530

    c

--- myapp/settings.py
+++ myapp/settings.py
@@ -123,10 +123,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATICFILES_DIRS = [
     BASE_DIR / "blog/static/blog/"
 ]
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
