bb7762f7cfc8c679bc756f73cf608d9c8ec12a57
Mohana Sri <mohanasri2606@gmail.com>
2025-03-13 15:04:21 +0530

    commit

--- myapp/settings.py
+++ myapp/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 import os
-import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -83,12 +82,17 @@ WSGI_APPLICATION = 'myapp.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(
-        default=os.getenv("DATABASE_URL"),
-        conn_max_age=600,  # Improves performance
-        ssl_require=True   # Required for secure PostgreSQL connection on Render
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'blog',
+        'USER': 'postgres',
+        'PASSWORD': 'Moya@1426',
+        'HOST': 'localhost',
+        'PORT': '5432'
+    }
+
 }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
