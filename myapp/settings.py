3197d9431dc5129b1a336fee1b9ac461a85a15e8
Mohana Sri <mohanasri2606@gmail.com>
2025-03-13 17:58:09 +0530

    Update settings.py

--- myapp/settings.py
+++ myapp/settings.py
@@ -83,10 +83,10 @@ WSGI_APPLICATION = 'myapp.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 
+# Get the DATABASE_URL from environment variables
 DATABASES = {
-    'default': dj_database_url.config(default='postgresql://postgres:PnkpsYuMyOgwyjrAzKOQJHLldhuIlcsf@nozomi.proxy.rlwy.net:25219/railway')
+    'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
